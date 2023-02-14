863eca54f7e551c3ddd854be9f5867db51987e7f
saikat <bsse0910@iit.du.ac.bd>
2023-02-14 05:10:40 +0000

    middle node

--- LeetAlgo/Program.cs
+++ LeetAlgo/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+ public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+ }
+
+
+public class Solution
+{
+    public ListNode MiddleNode(ListNode head)
+    {
+        return head;
+    }
+}
