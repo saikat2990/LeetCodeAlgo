8db3488cf6f928eb1eed019396f3f1fa8e912196
saikat <bsse0910@iit.du.ac.bd>
2023-02-14 16:07:01 +0600

    reverse link list

--- LeetAlgo/Program.cs
+++ LeetAlgo/Program.cs
@@ -1,30 +1,10 @@
 ﻿
- public class ListNode
-{
-    public int val;
-    public ListNode next;
-    public ListNode(int val = 0, ListNode next = null)
-    {
-        this.val = val;
-        this.next = next;
-    }
- }
 
+
+
+using LeetAlgo;
 
 public class Solution
 {
-    public ListNode MiddleNode(ListNode head)
-    {
-        var node = head;
-        List<ListNode> list = new List<ListNode>();
-
-        while (node!=null)
-        {
-            list.Add(node);
-            node = node.next;
-        }
-
-        var middlePoint = (list.Count / 2);
-        return list[middlePoint];
-    }
+    
 }
---
+++ LeetAlgo/ReverseLists.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetAlgo
+{
+
+
+    public class Solution
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+            ListNode resNode = null;
+            while (head != null)
+            {
+                resNode = new ListNode(head.val, resNode);
+                head = head.next;
+            }
+
+
+            return resNode;
+        }
+    }
+}
